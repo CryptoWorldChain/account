370cb27a3d0c9604df9000daedb5fd958ae29516
fanyangyang <a985850124@qq.com>
2018-06-05 21:48:41 +0700

    query blockentity from dao while can't found from this.blocks

--- src/main/java/org/brewchain/account/core/BlockChainStoreWithLRU.java
+++ src/main/java/org/brewchain/account/core/BlockChainStoreWithLRU.java
@@ -1,17 +1,25 @@
 package org.brewchain.account.core;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Provides;
+import org.brewchain.account.dao.DefDaos;
 import org.brewchain.account.gens.Block.BlockEntity;
 import org.brewchain.account.util.ALock;
 import org.brewchain.account.util.LRUCache;
+import org.brewchain.account.util.OEntityBuilder;
+import org.brewchain.bcapi.backend.ODBException;
+import org.brewchain.bcapi.gens.Oentity.OValue;
 import org.fc.brewchain.bcapi.EncAPI;
+
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -27,8 +35,11 @@ import onight.tfw.ntrans.api.annotation.ActorRequire;
 public class BlockChainStoreWithLRU implements ActorService {
 	@ActorRequire(name = "bc_encoder", scope = "global")
 	EncAPI encApi;
+	
+	@ActorRequire(name = "Def_Daos", scope = "global")
+	DefDaos dao;
 
-	protected final LRUCache<Integer, List<byte[]>> storage;
+	protected final LinkedHashMap<Integer, List<byte[]>> storage;
 	protected final LRUCache<String, BlockEntity> blocks;
 
 	protected ReadWriteLock rwLock = new ReentrantReadWriteLock();
@@ -38,7 +49,7 @@ public class BlockChainStoreWithLRU implements ActorService {
 	private final int CACHE_SIZE = 1000;
 
 	public BlockChainStoreWithLRU() {
-		this.storage = new LRUCache<Integer, List<byte[]>>(CACHE_SIZE);
+		this.storage = new LinkedHashMap<Integer, List<byte[]>>();
 		this.blocks = new LRUCache<String, BlockEntity>(CACHE_SIZE);
 	}
 
@@ -59,9 +70,10 @@ public class BlockChainStoreWithLRU implements ActorService {
 	public BlockEntity getBlockByNumber(int blockNumber) {
 		try (ALock l = readLock.lock()) {
 			List<byte[]> hashs = this.storage.get(blockNumber);
-			if (hashs != null) {
-				return this.blocks.get(encApi.hexEnc(hashs.get(0)));
-			}
+			if(hashs != null){
+				BlockEntity blockEntity = getBlockEntityByHash(hashs.get(0));
+				return blockEntity;
+			}				
 			return null;
 		}
 	}
@@ -111,6 +123,33 @@ public class BlockChainStoreWithLRU implements ActorService {
 			storage.clear();
 		}
 	}
+	
+	/**
+	 * 通过 hash 查询 BlockEntity 
+	 * 先从 this.blocks 中进行查询，如果查询不到，从数据库中进行查询
+	 * @param hash
+	 * @return
+	 */
+	public BlockEntity getBlockEntityByHash(byte[] hash){
+		if(hash != null){
+			BlockEntity blockEntity = this.blocks.get(encApi.hexEnc(hash));
+			if(blockEntity == null){
+				OValue v = null;
+				try {
+					v = dao.getBlockDao().get(OEntityBuilder.byteKey2OKey(hash)).get();
+					if(v != null){
+						blockEntity = BlockEntity.parseFrom(v.getExtdata());
+					}
+				} catch (ODBException | InterruptedException | ExecutionException | InvalidProtocolBufferException e) {
+					log.error(String.format("dao.getBlockDao().get(OEntityBuilder.byteKey2OKey(%s)).get()", hash));
+					return null;
+				}
+			}
+			return blockEntity;
+		} else {
+			return null;
+		}
+	}
 
 	public List<BlockEntity> getChildListBlocksEndWith(String blockHash, String endBlockHash, int maxCount) {
 		BlockEntity firstBlock = this.blocks.get(blockHash);
@@ -125,7 +164,7 @@ public class BlockChainStoreWithLRU implements ActorService {
 			List<byte[]> hashs = this.storage.get(number + i);
 			if (hashs != null) {
 				for (int j = 0; j < hashs.size(); j++) {
-					BlockEntity child = this.blocks.get(encApi.hexEnc(hashs.get(j)));
+					BlockEntity child = getBlockEntityByHash(hashs.get(j));
 					if (child != null) {
 						blocks.add(child);
 					}
@@ -152,7 +191,7 @@ public class BlockChainStoreWithLRU implements ActorService {
 			List<byte[]> hashs = this.storage.get(number - i);
 			if (hashs != null) {
 				for (int j = 0; j < hashs.size(); j++) {
-					BlockEntity child = this.blocks.get(encApi.hexEnc(hashs.get(j)));
+					BlockEntity child = getBlockEntityByHash(hashs.get(j));
 					if (child != null) {
 						blocks.add(child);
 					}
