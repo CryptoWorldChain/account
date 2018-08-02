ef7e29485d4cbb26fb87cfc21d10a63c02c7386e
sean <sean@finchain360.com>
2018-08-02 14:53:35 +0800

    update reward

--- src/main/java/org/brewchain/account/core/processor/V1Processor.java
+++ src/main/java/org/brewchain/account/core/processor/V1Processor.java
@@ -116,7 +116,7 @@ public class V1Processor implements IProcessor, ActorService {
 	@Override
 	public void applyReward(BlockEntity oCurrentBlock) throws Exception {
 		accountHelper.addTokenBalance(ByteString.copyFrom(encApi.hexDec(oCurrentBlock.getMiner().getAddress())), "CWS",
-				new BigInteger(String.valueOf(oCurrentBlock.getMiner().getReward())));
+				ByteUtil.bytesToBigInteger(oCurrentBlock.getMiner().getReward().toByteArray()));
 	}
 
 	@Override
