25eb8e4eb3ae5dbff7a34ad5a61c084f485af965
brew <brewcwv@gmail.com>
2018-08-20 00:08:24 +0800

    add just pending

---
+++ src/main/java/org/brewchain/account/sample/TransactionLoadTestPendingImpl.java
@@ -0,0 +1,82 @@
+package org.brewchain.account.sample;
+
+import org.brewchain.account.core.AccountHelper;
+import org.brewchain.account.core.BlockChainHelper;
+import org.brewchain.account.core.BlockHelper;
+import org.brewchain.account.core.TransactionHelper;
+import org.brewchain.account.gens.TxTest.PTSTCommand;
+import org.brewchain.account.gens.TxTest.PTSTModule;
+import org.brewchain.account.gens.TxTest.ReqCommonTest;
+import org.brewchain.account.gens.TxTest.RespCommonTest;
+import org.brewchain.account.gens.TxTest.RespCreateTransactionTest;
+import org.brewchain.evmapi.gens.Tx.MultiTransaction;
+import org.fc.brewchain.bcapi.EncAPI;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import onight.oapi.scala.commons.SessionModules;
+import onight.osgi.annotation.NActorProvider;
+import onight.tfw.async.CompleteHandler;
+import onight.tfw.ntrans.api.annotation.ActorRequire;
+import onight.tfw.otransio.api.PacketHelper;
+import onight.tfw.otransio.api.beans.FramePacket;
+
+@NActorProvider
+@Slf4j
+@Data
+public class TransactionLoadTestPendingImpl extends SessionModules<ReqCommonTest> {
+	@ActorRequire(name = "Block_Helper", scope = "global")
+	BlockHelper blockHelper;
+	@ActorRequire(name = "bc_encoder", scope = "global")
+	EncAPI encApi;
+	@ActorRequire(name = "BlockChain_Helper", scope = "global")
+	BlockChainHelper blockChainHelper;
+	@ActorRequire(name = "Account_Helper", scope = "global")
+	AccountHelper accountHelper;
+	@ActorRequire(name = "Transaction_Helper", scope = "global")
+	TransactionHelper transactionHelper;
+	@ActorRequire(name = "TransactionLoadTest_Store", scope = "global")
+	TransactionLoadTestStore transactionLoadTestStore;
+
+	@Override
+	public String[] getCmds() {
+		return new String[] { "PEN" };
+	}
+
+	@Override
+	public String getModule() {
+		return PTSTModule.TST.name();
+	}
+
+	@Override
+	public void onPBPacket(final FramePacket pack, final ReqCommonTest pb, final CompleteHandler handler) {
+		RespCreateTransactionTest.Builder oRespCreateTransactionTest = RespCreateTransactionTest.newBuilder();
+
+		String txHash = "";
+		try {
+			MultiTransaction.Builder tx = transactionLoadTestStore.getOne();
+			if (tx != null) {
+//				txHash = transactionHelper.CreateMultiTransaction(tx).getHexKey();
+				oRespCreateTransactionTest.setRetmsg("success");
+				oRespCreateTransactionTest.setTxhash(tx.getTxHash());
+				oRespCreateTransactionTest
+						.setFrom(encApi.hexEnc(tx.getTxBody().getInputs(0).getAddress().toByteArray()));
+				oRespCreateTransactionTest
+						.setTo(encApi.hexEnc(tx.getTxBody().getOutputs(0).getAddress().toByteArray()));
+
+				if (txHash.length() != 64) {
+					log.error("wrong txHash::" + txHash);
+				}
+			} else {
+				log.error("cannot find test case::");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error("test fail::", e);
+			oRespCreateTransactionTest.clear();
+			oRespCreateTransactionTest.setRetmsg("error");
+		}
+		handler.onFinished(PacketHelper.toPBReturn(pack, oRespCreateTransactionTest.build()));
+		return;
+	}
+}
